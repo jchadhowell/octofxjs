8c0d4cc2554b98df94fb292e73bdf8958b8a444f
jchadhowell <jchadhowell@gmail.com>
2016-12-28 14:55:08 -0600

    adding gulp tasks bump and publish

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,9 @@ var gulp = require('gulp'),
     rename = require('gulp-rename'),
     jsmin = require('gulp-jsmin'),
     concat = require('gulp-concat'),
-    clean = require('gulp-clean');
+    clean = require('gulp-clean'),
+    bump = require('gulp-bump'),
+    octo = require('@octopusdeploy/gulp-octo');
 
 gulp.task('less', function() {
   return gulp.src('./src/less/**/*.less')
@@ -34,4 +36,16 @@ gulp.task('default', ['less', 'scripts'], function() {
 gulp.task('clean-public', function () {
   return gulp.src(['./public/js', './public/css'], {read: false})
       .pipe(clean());
+});
+
+gulp.task('bump', function(){
+  return gulp.src('./package.json')
+  .pipe(bump({type: 'patch'}))
+  .pipe(gulp.dest('./'));
+});
+
+gulp.task('publish', ['bump', 'build'], function () {
+  return gulp.src(['**/*', '!bin{,/**}', '!src{,/**}', '!gulpfile.js'])
+      .pipe(octo.pack())
+      .pipe(octo.push({apiKey: 'API-IH48V7KALQKOKKKDUIM4KIYY8M', host: 'http://10.0.0.218/nuget/packages'}));
 });
